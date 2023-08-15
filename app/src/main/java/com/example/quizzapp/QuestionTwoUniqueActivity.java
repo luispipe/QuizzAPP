8f519a6068b5118d4145d6c7a06794afa9b75b17
luispipe <lflondonor@unal.edu.co>
2023-08-14 22:08:54 -0300

    first commit

---
+++ app/src/main/java/com/example/quizzapp/QuestionTwoUniqueActivity.java
@@ -0,0 +1,14 @@
+package com.example.quizzapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuestionTwoUniqueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_two_unique);
+    }
+}
